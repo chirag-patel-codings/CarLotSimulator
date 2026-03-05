9aeaa1aa4611c3effd717e022b344a3b04f656b6
Chirag Patel <chirag.patel.learning@gmail.com>
2026-03-05 13:27:26 -0600

    some changes

--- CarLotSimulatorApp/CarLotSimulator/CarLot.cs
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -8,11 +8,11 @@ namespace CarLotSimulator
     public class CarLot
     {
         
-        // Creating a Property 'ListOfCars' and Initiating with Default Value;
-        public List<Car> ListOfCars { get; set; } = new List<Car>();
-        
         // Static Keyword: Exercise 2
         public static int _numberOfCars = 0;
+        
+        // Creating a Property 'ListOfCars' and Initiating with Default Value;
+        public List<Car> ListOfCars { get; set; } = new List<Car>();
         
     }
 }
